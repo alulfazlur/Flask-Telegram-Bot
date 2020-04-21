219f805c6d2760bd8b66b08955ac90e826cf3361
Fazlur Rahman <fazlur@alterra.id>
2020-04-21 15:34:33 +0700

    Bot response

--- blueprints/weather/__init__.py
+++ blueprints/weather/__init__.py
@@ -12,12 +12,12 @@ class GetForecastWeather(Resource):
 	owm_host = "http://api.openweathermap.org/data/2.5/forecast"
 	owm_apikey = "a11dc66d804a26212628ebeb53c01a6a"
 
-	def get(self):
+	def get(self, city):
 		# parser = reqparse.RequestParser()
 		# parser.add_argument('q', location='args', default=None)
 		
 		# args = parser.parse_args()
-		city = 'Malang, ID'
+		# city = 'Malang, ID'
 
 		rq = requests.get(self.owm_host, params={'q': city, 'appid': self.owm_apikey})
 		forecast = rq.json()
@@ -39,15 +39,15 @@ class GetForecastWeather(Resource):
 				counter += 1
 			if counter >= 4 :
 				hasil['weather today'] = 'Mostly rain, you should bring your umbrella!'
-				rslt = 'Mostly rain, you should bring your umbrella!'
+				rslt = 'Mostly rain, you should bring your umbrella!'+ '\n'
 			elif counter == 0 :
 				hasil['weather today'] = 'Sun bright all day, no need to bring umbrella'
-				rslt = 'Sun bright all day, no need to bring umbrella'
+				rslt = 'Sun bright all day, no need to bring umbrella'+ '\n'
 			else :
 				hasil['weather today'] = 'Sometimes rain, maybe you should bring umbrella'
-				rslt = 'Sometimes rain, maybe you should bring umbrella\n'
+				rslt = 'Sometimes rain, maybe you should bring umbrella' + '\n'
 		hasil['city id'] = forecast['city']['id']
-		rslt += str(forecast['city']['id']) + '\n'
+		# rslt += str(forecast['city']['id']) + '\n'
 		hasil['city'] = forecast['city']['name']
 		rslt += forecast['city']['name'] + '\n'
 		hasil['date'] = str(today)
--- main.py
+++ main.py
@@ -2,55 +2,63 @@ import telebot, time, logging, sys
 
 TOKEN = '1112054330:AAGx68ug-fhaAhSAqoe4I92LWV6-j_gmWZA'
 bot = telebot.TeleBot(TOKEN)
+knownUsers = []  # todo: save these in a file,
+userStep = {}  # so they won't reset every time the bot restarts
 
-# def echo_messages(messages):
-#     """
-#     Echoes all incoming messages of content_type 'text'.
-#     """
-#     for m in messages:
-#         chatid = m.chat.id
-#         if m.content_type == 'text':
-#             text = m.text
-#             bot.send_message(chatid, text)
+commands = {  # command description used in the "help" command
+    'start'       : 'Get used to the bot',
+    'help'        : 'Gives you information about the available commands',
+    'forecast': "Forecast your city's weather",
+    'qod'    : 'Your Quotes of the day'
+}
 
+def listener(messages):
+    """
+    When new messages arrive TeleBot will call this function.
+    """
+    for m in messages:
+        if m.content_type == 'text':
+            # print the sent message to the console
+            print(str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + m.text)
 
-# Handle /start and /help
 @bot.message_handler(commands=['start'])
-def command_help(message):
-    bot.reply_to(message, "Hello, did someone call for help?")
+def command_start(m):
+    cid = m.chat.id
+    if cid not in knownUsers:  # if user hasn't used the "/start" command yet:
+        knownUsers.append(cid)  # save user id, so you could brodcast messages to all users of this bot later
+        userStep[cid] = 0  # save user id and his current "command level", so he can use the "/getImage" command
+        bot.send_message(cid, "Hello, stranger, let me scan you...")
+        bot.send_message(cid, "Scanning complete, I know you now")
+        bot.send_message(cid, "Hai " + m.chat.first_name)
+        command_help(m)  # show the new user the help page
+    else:
+        bot.send_message(cid, "I already know you, " + m.chat.first_name +", no need for me to scan you again!")
 
-
-# @bot.message_handler(commands=['help'])
-# def help_command(message):
-#    keyboard = telebot.types.InlineKeyboardMarkup()
-#    keyboard.add(
-#        telebot.types.InlineKeyboardButton(
-#            ‘Message the developer’, url='telegram.me/artiomtb'
-#        )
-#    )
-#    bot.send_message(
-#        message.chat.id,
-#        '1) To receive a list of available currencies press /exchange.\n' +
-#        '2) Click on the currency you are interested in.\n' +
-#        '3) You will receive a message containing information regarding the source and the target currencies, ' +
-#        'buying rates and selling rates.\n' +
-#        '4) Click “Update” to receive the current information regarding the request. ' +
-#        'The bot will also show the difference between the previous and the current exchange rates.\n' +
-#        '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
-#        reply_markup=keyboard
-#    )
+@bot.message_handler(commands=['help'])
+def command_help(m):
+    cid = m.chat.id
+    help_text = "The following commands are available: \n"
+    for key in commands:  # generate help text out of the commands dictionary defined at the top
+        help_text += "/" + key + ": "
+        help_text += commands[key] + "\n"
+    bot.send_message(cid, help_text)  # send the generated help page
 
 from blueprints.weather import GetForecastWeather
 @bot.message_handler(commands=['forecast'])
 def forecastWeather(message):
-    results = GetForecastWeather().get()
-    bot.reply_to(message, results)
+    sent = bot.send_message(message.chat.id, 'Masukkan kotamu')
+    bot.register_next_step_handler(sent, forecastSent)
+def forecastSent(message):
+    chatid = message.chat.id
+    text = message.text
+    results = GetForecastWeather().get(text)
+    bot.send_message(message.chat.id, results)
 
 from blueprints.qod import QuotesOfTheDay
 @bot.message_handler(commands=['qod'])
-def forecastWeather(message):
+def quotesOftheDay(message):
     results = QuotesOfTheDay().get()
-    bot.reply_to(message, results)
+    bot.send_message(message.chat.id, results)
 
 from blueprints.track import TracksOfTheDay
 @bot.message_handler(commands=['song'])
@@ -66,7 +74,6 @@ def forecastWeather(message):
     results = "Good morning, kawula muda!\n\n--Weather today--\n%s\n\n--Quotes of the day--\n%s\n\n--Song for you--%s" % (weather_of_the_day, quotes_of_the_day, track_of_the_day)
     bot.reply_to(message, results)
 
-# bot.set_update_listener(echo_messages)
 bot.polling()
 
 while True: # Don't let the main Thread end.
